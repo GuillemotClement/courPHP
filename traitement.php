1514fb53efde8b1745004695cbc232b0801eaf39
GuillemotClement <guillemot.clement@protonmail.com>
2023-02-23 16:52:15 +0100

    Ajout de l'exo age + 50

--- traitement.php
+++ traitement.php
@@ -1,8 +1,13 @@
 <?php
+    //Déclaration du tableau avec les infos de l'utilisateur
+    $user = array(
+        "id"        => 2,
+        "prenom"    => "Jean",
+        "nom"       => "PeuPlu",
+        "age"       => 40
+    );
 
-
-    $age = 18;
-    echo $age;
-    $echap = '"18"';
-    echo $echap;
-?>
+    //Affichage de l'age de la personne dans 50 ans
+    $futur = $user["age"] + 50;
+    echo "Bonjour ".$user["prenom"]." ".$user["nom"]." ! "."Dans 50 ans vous aurez : ".$futur;
+?> 
